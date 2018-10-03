0a294108359ebe02f4d44a3d04a18562541dacb5
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-03 15:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,4 +29,12 @@ function createPost() {
   let postTemplate = document.getElementById('post-template').innerHTML;
   
   let templateFn = _.template(postTemplate);
+  
+  let postsDiv = document.getElementById('posts');
+ 
+  //execute template function with JSON object for the interpolated values
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+ 
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
 }
